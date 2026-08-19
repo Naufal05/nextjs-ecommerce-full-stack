9bbbe9f86dfa8e093e5099e4de9f14e69959184f
Naufal05 <mnaufalmz@gmail.com>
2026-08-19 13:27:58 +0530

    admin-card-component

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,3 +1,40 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function AdminDashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <div className="grid grid-cols-1 md: grid-cols-2 lg:grid-cols-3 gap-4">
+        <DashboardCard title="Sales" subtitle="Test" body="body" />
+        <DashboardCard title="Sales" subtitle="Test" body="body" />
+        <DashboardCard title="Sales" subtitle="Test" body="body" />
+        <DashboardCard title="Sales" subtitle="Test" body="body" />
+      </div>
+    </div>
+  );
+}
+
+type DashboardCardProps = {
+  title: string;
+  subtitle: string;
+  body: string;
+};
+
+function DashboardCard({ title, subtitle, body }: DashboardCardProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Sales</CardTitle>
+        <CardDescription>Desc</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{body}</p>
+      </CardContent>
+    </Card>
+  );
 }
